db14f5e16e235936c6871528974fef0310232008
Daniel Krastev <danielkrastev01@gmail.com>
2025-08-14 23:13:57 +0300

    Get and Create Cateogory

---
+++ src/controllers/categories-controller.ts
@@ -0,0 +1,55 @@
+import { Router, Request, Response } from "express";
+import categoriesService from "../services/categories-service.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
+
+const categoriesController = Router();
+
+categoriesController.get('/', async (req: Request, res: Response) => {
+    try {
+        let data;
+        let limitRaw = req.query.limit;
+
+        let limit: number | undefined;
+
+        if (limitRaw !== undefined) {
+            if (typeof limitRaw === 'string') {
+                limit = parseInt(limitRaw);
+            } else if (typeof limitRaw === 'number') {
+                limit = limitRaw;
+            } else {
+                // if array or ParsedQs - error
+                return res.status(400).json({ error: 'Invalid limit parameter' });
+            }
+
+            if (isNaN(limit)) {
+                return res.status(400).json({ error: 'Limit must be a valid number' });
+            }
+        }
+
+        if (limit !== undefined) {
+            data = await categoriesService.getAllLimit(limit);
+        } else {
+            data = await categoriesService.getAll();
+        }
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+});
+
+categoriesController.post('/', async (req: Request, res: Response) => {
+    
+    try{
+        const data: object = req.body;
+
+        const createdData = await categoriesService.create(data);
+        res.status(200).json(createdData);
+    }catch(error) {
+        const errorMessage = getErrorMessage(error);
+        return res.status(400).json({ error: errorMessage });
+    }
+
+});
+
+export default categoriesController;
