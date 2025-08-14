165cce29c757ff6353c41848521d6cb804b7eb73
Daniel Krastev <danielkrastev01@gmail.com>
2025-08-14 23:16:08 +0300

    Create new route for Categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import paintingsController from "./controllers/paintings-controller.js";
+import categoriesController from "./controllers/categories-controller.js";
 
 const routes = Router();
 
 routes.use('/paintings', paintingsController);
+routes.use('/categories', categoriesController);
 
 export default routes;
