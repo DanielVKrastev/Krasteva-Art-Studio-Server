a173b5351475dd1b823c9fa096c255344c0dc202
Daniel Krastev <danielkrastev01@gmail.com>
2025-08-16 21:55:30 +0300

    Added new route for sizes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import paintingsController from "./controllers/paintings-controller.js";
 import categoriesController from "./controllers/categories-controller.js";
+import sizesController from "./controllers/sizes-controller.js";
 
 const routes = Router();
 
 routes.use('/paintings', paintingsController);
 routes.use('/categories', categoriesController);
+routes.use('/sizes', sizesController);
 
 export default routes;
