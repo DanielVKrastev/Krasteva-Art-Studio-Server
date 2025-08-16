0bbe838553868f6a756268234263ec44b7fa4f13
Daniel Krastev <danielkrastev01@gmail.com>
2025-08-16 21:52:24 +0300

    Create sizes-service.ts

---
+++ src/services/sizes-service.ts
@@ -0,0 +1,30 @@
+import Size from "../models/size-model.js";
+
+export default {
+    async getAll(){
+        const data = await Size.find().sort( { createdAt: 1 } );
+        return data;
+    },
+    async getOne(id: string){
+        const data = await Size.findOne({ _id: id});
+        return data;
+    },
+    async create(data: object){
+
+        const creadtedData = await Size.create(data);
+        return creadtedData;
+    },
+    async update(id: string, updateData: object){
+        const updateContact = await Size.findByIdAndUpdate(id, updateData, {new: true, runValidators: true });
+        
+        return this.getOne(updateContact?.id);
+    },
+    async delete(id: String){
+        return await Size.findByIdAndDelete(id);
+    },
+    async getAllLimit(limit: number){
+        return await Size.find()
+                .sort({ createdAt: -1 })  
+                .limit(limit);           
+    }
+}
