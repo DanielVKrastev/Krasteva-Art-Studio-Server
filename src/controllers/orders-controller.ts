68fa79986afeea2864b2eff2af73e35500f5627c
Daniel Krastev <danielkrastev01@gmail.com>
2025-08-21 21:55:36 +0300

    Create Order Controller
    Get all and Create

---
+++ src/controllers/orders-controller.ts
@@ -0,0 +1,56 @@
+import { Router, Request, Response } from "express";
+
+import { getErrorMessage } from "../utils/errorUtils.js";
+import ordersService from "../services/orders-service.js";
+
+const ordersController = Router();
+
+ordersController.get('/', async (req: Request, res: Response) => {
+    try {
+        let data;
+        let limitRaw = req.query.limit;
+
+        let limit: number | undefined;
+
+        if (limitRaw !== undefined) {
+            if (typeof limitRaw === 'string') {
+                limit = parseInt(limitRaw);
+            } else if (typeof limitRaw === 'number') {
+                limit = limitRaw;
+            } else {
+                // if array or ParsedQs - error
+                return res.status(400).json({ error: 'Invalid limit parameter' });
+            }
+
+            if (isNaN(limit)) {
+                return res.status(400).json({ error: 'Limit must be a valid number' });
+            }
+        }
+
+        if (limit !== undefined) {
+            data = await ordersService.getAllLimit(limit);
+        } else {
+            data = await ordersService.getAll();
+        }
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+});
+
+ordersController.post('/', async (req: Request, res: Response) => {
+    
+    try{
+        const data: object = req.body;
+
+        const createdData = await ordersService.create(data);
+        res.status(200).json(createdData);
+    }catch(error) {
+        const errorMessage = getErrorMessage(error);
+        return res.status(400).json({ error: errorMessage });
+    }
+
+});
+
+export default ordersController;
