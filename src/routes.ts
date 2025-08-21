5c180730829e4ab296d5a3cd430df7923c485575
Daniel Krastev <danielkrastev01@gmail.com>
2025-08-21 21:56:46 +0300

    New route for orders

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import paintingsController from "./controllers/paintings-controller.js";
 import categoriesController from "./controllers/categories-controller.js";
 import sizesController from "./controllers/sizes-controller.js";
 import aboutController from "./controllers/about-controller.js";
+import ordersController from "./controllers/orders-controller.js";
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/paintings', paintingsController);
 routes.use('/categories', categoriesController);
 routes.use('/sizes', sizesController);
 routes.use('/about', aboutController);
+routes.use('/orders', ordersController);
 
 export default routes;
